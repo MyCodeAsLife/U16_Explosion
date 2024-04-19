5b987f1b36d1b5d6f0e55e0f23581651ef02c7e9
Vladimir <w112vovan@mail.ru>
2024-04-19 15:46:32 +0500

    0.2

--- Assets/Scripts/CubeSpawner.cs
+++ Assets/Scripts/CubeSpawner.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(AudioSource))]
-public class ObjectPoolManager : MonoBehaviour
+public class CubeSpawner : MonoBehaviour
 {
     private ParticleSystem _prefabExplosion;
     private BaseCube _prefabObject;
